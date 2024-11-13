8b3e2099348e0ff811b96f310e5932530de54cca
Keely Lowder <keelybowman@icloud.com>
2024-11-13 15:30:33 -0500

    initial commit

---
+++ src/main/java/com/pluralsight/deli/model/Product.java
@@ -0,0 +1,28 @@
+package com.pluralsight.deli.model;
+
+public abstract class Product {
+    protected String name;
+    private double price;
+
+
+    // Constructor
+    public Product(String name, double price) {
+        this.name = name;
+        this.price = price;
+    }
+
+    // Getter for name
+    public String getName() {
+        return name;
+    }
+
+    // Setter for name
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    // Abstract method because each product will have a difference price
+    public abstract double getPrice();
+    }
+
+
---
+++ src/main/java/com/pluralsight/deli/model/enums/Sauces.java
@@ -0,0 +1,4 @@
+package com.pluralsight.deli.model.enums;
+
+public enum Sauces {
+}
