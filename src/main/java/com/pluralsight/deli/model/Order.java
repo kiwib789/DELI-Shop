4b5db1124e7cdf6fdd988a347c35d6dc957baa8f
Keely Lowder <keelybowman@icloud.com>
2024-11-14 06:36:23 -0500

    working on adding date and time to receipt

--- src/main/java/com/pluralsight/deli/model/Order.java
+++ src/main/java/com/pluralsight/deli/model/Order.java
@@ -1,16 +1,20 @@
 package com.pluralsight.deli.model;
 
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
+
 public class Order {
     private int id;
     private String customer;
     private Sandwich sandwich;
-    private double price;
+    private double totalPrice;
 
-    public Order(int id, String customer, Sandwich sandwich, double price) {
+    public Order(int id, String customer, Sandwich sandwich, double totalPrice) {
         this.id = id;
         this.customer = customer;
         this.sandwich = sandwich;
-        this.price = price;
+        this.totalPrice = totalPrice;
     }
 
     public int getId() {
@@ -44,6 +48,9 @@ public class Order {
     public void setPrice(double price) {
         this.price = price;
     }
+
+   DateTimeFormatter format = new DateTimeFormatterBuilder("yyyyMMdd-HHmmss");
+        this.dateTime = LocalDateTime.format(format);
 }
 // add item
 // get total price
